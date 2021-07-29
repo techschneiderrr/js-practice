290c2197240e537ea4ce9467e3a3cb5e10e79ca2
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-07-30 00:52:29 +0530

    cakc

--- js/index.js
+++ js/index.js
@@ -1,7 +1,34 @@
 
-function submit()
-{
-    var color = document.getElementById("color").value;
-    document.querySelector("html").style.backgroundColor= color;
-    document.querySelector("body").style.backgroundColor= color;
+// function submit()
+// {
+//     var pass = document.getElementById("inputPassword4").value;
+//     console.log(pass);
+   
+// }
+
+function new_element(c){
+    var node = document.getElementById('new');
+    var code = `<label for="result"><b>Result</b></label><div id="result">${c}</div>`;
+    node.innerHTML=code;
 }
+
+function add(a,b){
+    c = a + b;
+    new_element(c);
+}
+
+function sub(a,b){
+    c = a - b;
+    new_element(c);
+}
+
+function mul(a,b){
+    c = a * b;
+    new_element(c);
+}
+
+function div(a,b){
+    c = a / b;
+    new_element(c);
+}
+
