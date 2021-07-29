ba05a55d89626185e25526c2bb2869e9fc0fc81d
Manav Deep Singh Lamba <56251839+techschneiderrr@users.noreply.github.com>
2021-07-29 17:12:56 +0530

    initial comit

---
+++ js/index.js
@@ -0,0 +1,7 @@
+
+function submit()
+{
+    var color = document.getElementById("color").value;
+    document.querySelector("html").style.backgroundColor= color;
+    document.querySelector("body").style.backgroundColor= color;
+}
